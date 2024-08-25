b09a8e6b8305ceccc3d4eec44fbb0f9dc74ce1a3
Ernstjan Hoek <ernstjanhoek@tutanota.com>
2024-08-25 16:47:06 +0200

    adminService getUsers observable

--- antifraud/src/app/admin-panel/admin-panel.component.ts
+++ antifraud/src/app/admin-panel/admin-panel.component.ts
@@ -1,6 +1,9 @@
 import {Component, inject} from '@angular/core';
 import {UserService} from "../user.service";
 import {AsyncPipe} from "@angular/common";
+import {AdminService} from "../admin.service";
+import {UserData} from "../user-data";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-admin-panel',
@@ -12,6 +15,13 @@ import {AsyncPipe} from "@angular/common";
   styleUrl: './admin-panel.component.css'
 })
 export class AdminPanelComponent {
-  userService: UserService = inject(UserService);
+
+  adminService: AdminService = inject(AdminService);
+
+  userList: Observable<UserData[]> | undefined;
+
+  getUsers() {
+    this.userList = this.adminService.getUsers();
+  }
 
 }
--- antifraud/src/app/admin.service.ts
+++ antifraud/src/app/admin.service.ts
@@ -1,9 +1,27 @@
-import { Injectable } from '@angular/core';
+import {inject, Injectable} from '@angular/core';
+import {HttpClient, HttpResponse} from "@angular/common/http";
+import {UserService} from "./user.service";
+import {map, Observable} from "rxjs";
+import {UserData} from "./user-data";
 
 @Injectable({
   providedIn: 'root'
 })
-export class AdminService {
 
-  constructor() { }
+export class AdminService {
+  userService: UserService = inject(UserService);
+
+  constructor(private http: HttpClient) {}
+
+  getUsers(): Observable<UserData[]> {
+    return this.http.get<UserData[]>(this.userService.url + "/api/auth/list", {
+      headers: {
+        'Authorization' : `Basic ${this.userService.encodedCredentials}`
+      },
+      observe: 'response'
+    })
+      .pipe(map((res:HttpResponse<UserData[]>) => {
+        return res.body;
+      }))
+  }
 }
